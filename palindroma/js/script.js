f2a5544ab5f15377c76c1d581d709dbc849b7945
Rajbir-Singh-Create <rajbir.singh.create@gmail.com>
2025-01-03 12:37:54 +0100

    feat: exercise completed

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,1 +1,28 @@
-console.log("Ciao");
+// inizializzo le variabili
+let word = "";
+let reverseWord = "";
+
+// faccio inserire la parola all'utente
+word = prompt("Inserisci una parola");
+word = word.toLowerCase();
+console.log(word);
+
+function palindromeCheck(){
+    // trasformo la stringa inserita dall'utente in un array
+    const convertedWord = word.split("");
+    // console.log(convertedWord);
+
+    // inverto l'ordine degli elementi all'interno dell'array e lo ritrasformo in stringa
+    reverseWord = convertedWord.reverse().join("").toLowerCase();
+    console.log(reverseWord);
+
+    // controllo se la parla inserita è palindroma
+    if(word === reverseWord){
+        console.log("la parola è palindroma");
+    } else {
+        console.log("la parola non è palindroma")
+    }
+}
+
+// chiamo la funzione
+palindromeCheck();
