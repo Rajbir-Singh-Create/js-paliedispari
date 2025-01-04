fc82f531b2d615974b994247fc6b2161bf454759
Rajbir-Singh-Create <rajbir.singh.create@gmail.com>
2025-01-04 13:35:59 +0100

    feat: exercise completed

--- pari-e-dispari/js/script.js
+++ pari-e-dispari/js/script.js
@@ -1,28 +1,35 @@
 // Dichiarazione variabili
 let macNum;
+let userNum;
 
 // Inserimento dei dati dall'utente
 let evenOrOdd = prompt("Pari o dispari?")
 evenOrOdd = evenOrOdd.toLowerCase();
-console.log(evenOrOdd);
+console.log(`Hai scelto ${evenOrOdd}`);
 
 // Controllo degli input inseriti dall'utente
-// TODO: implementare con una funzione?
 if(evenOrOdd === "dispari" || evenOrOdd === "pari"){
-    // chiamo la funzione per proseguire il gioco
+    // Proseguo con l'inserimento del numero dall'utente
+    userNumberCheck();
 } else {
     alert("inserisci un valore valido");
 }
 
-// Inserimento dei dati dall'utente
-let userNum = prompt("inserisci un numero tra 1 e 5");
-console.log(userNum);
 
-// Controllo degli input inseriti dall'utente
-if(userNum > 0 && userNum <= 5){
-    // chiamo la funzione per proseguire
-} else {
-    alert("Inserisci un numero valido, tra 1 e 5");
+function userNumberCheck() {
+    // Inserimento dei dati dall'utente
+    userNum = prompt("inserisci un numero tra 1 e 5");
+    userNum = parseInt(userNum);
+    console.log(`Numero inserito dall'utente: ${userNum}`);
+    console.log(`Numero generato dalla macchina: ${machineNum(1, 5)}`);
+
+    // Controllo degli input inseriti dall'utente
+    if(userNum > 0 && userNum <= 5){
+        // chiamo la funzione per proseguire il gioco
+        isEvenOrOdd();
+    } else {
+        alert("Inserisci un numero valido, tra 1 e 5");
+    }
 }
 
 
@@ -31,13 +38,24 @@ function machineNum(min, max){
         macNum = Math.round(Math.random() * (max - min) + min);
         return macNum;
 }
-console.log(machineNum(1, 5));
 
 
-// Somma dei due numeri
-const sum = macNum + userNum;
-
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto
 function isEvenOrOdd(){
-    
+    // Somma dei due numeri
+    const sum = macNum + userNum;
+    console.log(`La somma dei numeri: ${sum}`);
+
+    const mod = 0;
+    // Controllo se il numero è pari o dispari
+    if(evenOrOdd === "pari" && mod === sum % 2){
+        alert("Hai vinto!");
+    } else if (evenOrOdd === "dispari" && mod !== sum % 2){
+        alert("Hai vinto!")
+    } else if (evenOrOdd === "pari" && mod !== sum % 2){
+        alert("Hai perso!");
+    } else if (evenOrOdd === "dispari" && mod === sum % 2){
+        alert("Hai perso!");
+    }
 }
