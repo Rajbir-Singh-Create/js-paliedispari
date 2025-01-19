4f02824f0f112ad8651600f00364289b536989c6
Rajbir-Singh-Create <rajbir.singh.create@gmail.com>
2025-01-19 12:37:56 +0100

    fix: remainder bug fix

--- bonus/pari-e-dispari/js/script.js
+++ bonus/pari-e-dispari/js/script.js
@@ -1,11 +1,13 @@
 // Dichiarazione variabili
 const inputForm = document.querySelector(".inputForm");
+const outputElements = document.querySelector(".outputElements");
 let selectedOption = document.querySelector(".selectOpt");
 let userNum = document.querySelector(".userNumber");
 let machineNum = document.querySelector(".machineNumberResult");
 let sumResult = document.querySelector(".sumResult");
-let finalResult = document.querySelector(".finalResult");
+let finalRes = document.querySelector(".finalResult");
 const wrongInput = document.querySelector(".wrongInput");
+let machineNumber = 0;
 
 // Al click del bottone / invio del form
 inputForm.addEventListener("submit", function (event) {
@@ -18,50 +20,53 @@ inputForm.addEventListener("submit", function (event) {
 // Elaborazione dei dati inseriti
 function userInput() {
     // Ottengo il valore dell'opzione selezionata
-    selectedOption = selectedOption.value;
+    let selectOpt = selectedOption.value;
 
     // Ottengo l'input inserito dall'utente e lo converto in un dato di tipo int
-    userNum = userNum.value;
-    userNum = parseInt(userNum);
+    let userNumber = userNum.value;
+    userNumber = parseInt(userNumber);
 
     // Controllo validità degli input inseriti dall'utente
-    if (userNum > 0 && userNum <= 5) {
+    if (userNumber > 0 && userNumber <= 5) {
         // Chiamo la funzione per generare il numero dalla macchina e lo assegno alla variabile
-        machineNum.value = `Numero generato dalla macchina: ${machineNumber(1, 5)}`;
+        machineNum.value = `Numero generato dalla macchina: ${machineNumberCalc(1, 5)}`;
         
         // Chiamo la funzione che esegue la somma dei due numeri
-        sumResult.value = `La somma dei numeri è: ${numSum(machineNum, userNum)}`;
+        let sumRes = numSumCalc(machineNumber, userNumber);
+        sumResult.value = `La somma dei numeri è: ${sumRes}`;
 
         // Chiamo la funzione per decidere se la somma è pari o dispari
-        finalResult.value = isEvenOrOdd(selectedOption, sumResult);
+        isEvenOrOdd(selectOpt, sumRes);
+
+        outputElements.classList.remove("d-none");
+        wrongInput.classList.add("d-none");
     } else {
-        sumResult.classList.add("d-none");
-        machineNum.classList.add("d-none");
+        outputElements.classList.add("d-none");
         wrongInput.classList.remove("d-none");
-        finalResult.classList.add("d-none");
     }
 }
 
-
 // Generazione numero random dalla macchina con una funzione
-function machineNumber(min, max) {
-    machineNum = Math.round(Math.random() * (max - min) + min);
-    return machineNum;
+function machineNumberCalc(min, max) {
+    machineNumber = Math.round(Math.random() * (max - min) + min);
+    return machineNumber;
 }
 
 // Somma del numero inserito dall'utente con quello generato dalla macchina
-function numSum(num1, num2){
-    sumResult = num1 + num2;
-    return sumResult;
+function numSumCalc(num1, num2){
+    let sumRes = num1 + num2;
+    return sumRes;
 }
 
 // Stabiliamo se la somma dei due numeri è pari o dispari
 function isEvenOrOdd(option, result) {
     const mod = 0;
 
-    if ((option === "pari" && mod === result % 2) || (option === "dispari" && mod !== result % 2)) {
-        return finalResult = "Hai vinto!";
-    } else if ((option === "pari" && mod !== result % 2) || (option === "dispari" && mod === result % 2)) {
-        return finalResult = "Hai perso!";
+    if ((option === "pari" && result % 2 === mod) || (option === "dispari" && result % 2 !== mod)) {
+        finalRes.value = "Hai vinto!";
+        return finalRes;
+    } else if ((option === "pari" && result % 2 !== mod) || (option === "dispari" && result % 2 === mod)) {
+        finalRes.value = "Hai perso!";
+        return finalRes;
     }
 }
